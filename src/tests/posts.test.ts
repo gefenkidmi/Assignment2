320ab5b50a23b064edc650efa4a98f743e793a27
Gefen Kidmi <gefenkidmi@Bens-Mac-mini.local>
2024-12-27 12:11:30 +0200

    Add comments model, routes, controller and tests

--- src/tests/posts.test.ts
+++ src/tests/posts.test.ts
@@ -1,25 +1,27 @@
 import initApp from "../server";
 import mongoose from "mongoose";
 import postModel from "../models/post_model";
+import commentsModel from "../models/comments_model";
 import { Express } from "express";
 import request from "supertest";
 
 let app: Express;
 
-
 beforeAll(async () => {
   console.log("beforeAll");
   app = await initApp();
+});
+
+beforeEach(async () => {
   await postModel.deleteMany();
+  await commentsModel.deleteMany();
 });
 
-afterAll((done) => {
+afterAll(async () => {
   console.log("afterAll");
-  mongoose.connection.close();
-  done();
+  await mongoose.connection.close();
 });
 
-let postId = "";
 describe("Posts Tests", () => {
   test("Posts test get all", async () => {
     const response = await request(app).get("/posts");
@@ -28,19 +30,23 @@ describe("Posts Tests", () => {
   });
 
   test("Test Create Post", async () => {
-    const response = await request(app).post("/posts")
-      .send({
-        title: "Test Post",
-        content: "Test Content",
-        owner: "TestOwner",
-      });
+    const response = await request(app).post("/posts").send({
+      title: "Test Post",
+      content: "Test Content",
+      owner: "TestOwner",
+    });
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe("Test Post");
     expect(response.body.content).toBe("Test Content");
-    postId = response.body._id;
   });
 
   test("Test get post by owner", async () => {
+    await request(app).post("/posts").send({
+      title: "Test Post",
+      content: "Test Content",
+      owner: "TestOwner",
+    });
+
     const response = await request(app).get("/posts?owner=TestOwner");
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(1);
@@ -49,40 +55,64 @@ describe("Posts Tests", () => {
   });
 
   test("Test get post by id", async () => {
-    const response = await request(app).get("/posts/" + postId);
+    const createResponse = await request(app).post("/posts").send({
+      title: "Test Post",
+      content: "Test Content",
+      owner: "TestOwner",
+    });
+    const createdPostId = createResponse.body._id;
+
+    const response = await request(app).get(`/posts/${createdPostId}`);
     expect(response.statusCode).toBe(200);
     expect(response.body.title).toBe("Test Post");
     expect(response.body.content).toBe("Test Content");
   });
 
   test("Test Create Post 2", async () => {
-    const response = await request(app).post("/posts")
-      .send({
-        title: "Test Post 2",
-        content: "Test Content 2",
-        owner: "TestOwner2",
-      });
+    const response = await request(app).post("/posts").send({
+      title: "Test Post 2",
+      content: "Test Content 2",
+      owner: "TestOwner2",
+    });
     expect(response.statusCode).toBe(201);
   });
 
   test("Posts test get all 2", async () => {
+    await request(app).post("/posts").send({
+      title: "Test Post 1",
+      content: "Content 1",
+      owner: "Owner 1",
+    });
+    await request(app).post("/posts").send({
+      title: "Test Post 2",
+      content: "Content 2",
+      owner: "Owner 2",
+    });
+
     const response = await request(app).get("/posts");
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(2);
   });
 
   test("Test Delete Post", async () => {
-    const response = await request(app).delete("/posts/" + postId)
-    expect(response.statusCode).toBe(200);
-    const response2 = await request(app).get("/posts/" + postId);
-    expect(response2.statusCode).toBe(404);
+    const createResponse = await request(app).post("/posts").send({
+      title: "Test Post",
+      content: "Test Content",
+      owner: "TestOwner",
+    });
+    const createdPostId = createResponse.body._id;
+
+    const deleteResponse = await request(app).delete(`/posts/${createdPostId}`);
+    expect(deleteResponse.statusCode).toBe(200);
+
+    const getResponse = await request(app).get(`/posts/${createdPostId}`);
+    expect(getResponse.statusCode).toBe(404);
   });
 
   test("Test Create Post fail", async () => {
-    const response = await request(app).post("/posts")
-      .send({
-        content: "Test Content 2",
-      });
+    const response = await request(app).post("/posts").send({
+      content: "Test Content 2",
+    });
     expect(response.statusCode).toBe(400);
   });
 });
